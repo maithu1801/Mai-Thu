29b0e8d7b508391ddf72ce3cc005f06a06580827
Thu <maithu1801tv@gmail.com>
2022-06-06 09:08:01 +0700

    demo git

---
+++ NotBug/src/main/java/com/example/NotBug/NotBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotBug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotBugApplication.class, args);
+	}
+
+}
---
+++ NotBug/src/test/java/com/example/NotBug/NotBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotBug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
